3de56102301e3f6cadf45ef6febcf08992cdbef1
Guillermo Rodriguez Sainz <guillermo.sainz@live.u-tad.com>
2017-03-13 23:20:45 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 var xmlhttp = new XMLHttpRequest();
-var url = "json/datiles.json";
+var url = "json/info.json";
 
 xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         var myArr = JSON.parse(this.responseText);
-        myFunction(myArr);
+        Div(myArr);
+        
     }
 };
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
 
-function myFunction(arr) {
+function Div(arr) {
     var section = document.getElementById("idContainer");
 
     for (i = 0; i < arr.length; i++) {
-         
          //Div principal
          var divProy = document.createElement("div");
          divProy.setAttribute("class", "proyecto");
          //Titulo
          var title = document.createElement("h3");
          title.setAttribute("class", "h3class");
-         var label = document.createTextNode(arr[i].display);
+         var label = document.createTextNode(arr[i].titulo);
          //Imagen
          var imagen = document.createElement("img");
          imagen.setAttribute("class", "imgProy");
@@ -32,11 +32,12 @@ function myFunction(arr) {
          var link = document.createElement("a");
          link.setAttribute("class", "enlaceClass");
          link.setAttribute("href", arr[i].source);
-
+        
          //CREACION DE LOS HIJOS DENTRO DE LOS DIVS
          idContainer.appendChild(divProy);
          divProy.appendChild(title);
-         divProy.appendChild(imagen);
+         title.appendChild(label);
+         link.appendChild(imagen);
          divProy.appendChild(link);
      }
 
