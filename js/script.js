c50e6aa813035d7c6dd91e57caed7cb6418d8168
Guillermo Rodriguez Sainz <guillermo.sainz@live.u-tad.com>
2017-03-13 22:42:21 +0100

    2

--- js/script.js
+++ js/script.js
@@ -1,49 +1,43 @@
-
 var xmlhttp = new XMLHttpRequest();
-var url = "json/data.json";
+var url = "json/datiles.json";
 
 xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-        var documentJson = JSON.parse(this.responseText);
-        var myArr = documentJson.trabajos;
+        var myArr = JSON.parse(this.responseText);
         myFunction(myArr);
     }
 };
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
+
 function myFunction(arr) {
     var section = document.getElementById("idContainer");
 
     for (i = 0; i < arr.length; i++) {
-        //Div principal
-        var proyecto = document.createElement("div");
+         
+         //Div principal
+         var divProy = document.createElement("div");
+         divProy.setAttribute("class", "proyecto");
+         //Titulo
+         var title = document.createElement("h3");
+         title.setAttribute("class", "h3class");
+         var label = document.createTextNode(arr[i].display);
+         //Imagen
+         var imagen = document.createElement("img");
+         imagen.setAttribute("class", "imgProy");
+         imagen.setAttribute("src", arr[i].imagen);
+         
+         //Enlace
+         var link = document.createElement("a");
+         link.setAttribute("class", "enlaceClass");
+         link.setAttribute("href", arr[i].source);
 
-        //Titulo
-        var titulo = document.createElement("h1");
-        //Texto dentro del h4
-        var setTitle = document.createTextNode(arr[i].titulo);
-
-        //imagenes
-        var pic = document.createElement("img");
-        pic.setAttribute("src", arr[i].imagen);
-
-        //enlace
-        var enlace = document.createElement("a");
-        enlace.setAttribute("href", arr[i].source);
-
-        //Insercion de los hijos en los elementos de la web
-
-        section.appendChild(proyecto);
-
-        proyecto.appendChild(titulo);
-        titulo.appendChild(setTitle);
-
-        proyecto.appendChild(pic);
-
-        proyecto.appendChild(enlace);
-    }
-
-
+         //CREACION DE LOS HIJOS DENTRO DE LOS DIVS
+         idContainer.appendChild(divProy);
+         divProy.appendChild(title);
+         divProy.appendChild(imagen);
+         divProy.appendChild(link);
+     }
 
 }
